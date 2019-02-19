5ba345adc148d08a0e4ae0a1122b52f6bb2077b9
benjas06 <benjaminne15@hotmail.com>
2019-02-19 16:46:10 -0600

    creamos archivos de rutas

---
+++ resources/views/paginas/contacto.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>contacto</title>
+</head>
+<body>
+    <h1>contacto</h1>
+</body>
+</html>
